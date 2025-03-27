c3f996ea66054d7aa9d14bfa7ef3e02125bd7d9c
BENALI Redhouane <redx.ben02@gmail.com>
2025-03-27 10:06:14 +0100

    Exercice 1 TP0

---
+++ src/test/java/PersonTest/PersonTest.java
@@ -0,0 +1,26 @@
+package PersonTest;
+
+import org.example.Person;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PersonTest {
+
+    @Test
+    void testGetFullName() {
+        Person person = new Person("Benali", "Redhouane", 25);
+        assertEquals("Benali Redhouane", person.getFullName());
+    }
+
+    @Test
+    void testIsAdultTrue() {
+        Person person = new Person("Benali", "Mohammed", 20);
+        assertTrue(person.isAdult());
+    }
+
+    @Test
+    void testIsAdultFalse() {
+        Person person = new Person("Benali", "abdelwaheb", 16);
+        assertFalse(person.isAdult());
+    }
+}
